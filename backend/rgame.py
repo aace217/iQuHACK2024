7a6aac6246977e6838e3d5fa68090e07a1814685
aace217 <aace217@mit.edu>
2024-02-04 01:47:32 -0500

    Started rgame.py

--- backend/rgame.py
+++ backend/rgame.py
@@ -1,46 +1,91 @@
 from qiskit import QuantumCircuit, Aer, execute
 import random
+import numpy as np
 
-def measure_qubit(qc, qubit_to_measure, cbit):
+NUM_CHARACTERS = 4
+TRAITS_PER_CHARACTER = 5
+NUM_QUBITS = NUM_CHARACTERS * TRAITS_PER_CHARACTER
+
+colors = ['red', 'blue', 'orange', 'green', 'black']
+trait_color_pairs = {}
+
+def generate_color_pairs():
+    all_color_pairs = [(colors[i], colors[j]) for i in range(len(colors)) for j in range(i+1, len(colors))]
+    random.shuffle(all_color_pairs)
+    for trait in range(TRAITS_PER_CHARACTER):
+        trait_color_pairs[trait] = all_color_pairs.pop()
+
+def entangle_corresponding_qubits_correctly(qc, num_characters, traits_per_character):
+    generate_color_pairs()  
+    for trait in range(traits_per_character):
+        qubit_indices = [trait + i*traits_per_character for i in range(num_characters)]
+        random.shuffle(qubit_indices)
+        while qubit_indices:
+            qubit1 = qubit_indices.pop()
+            if qubit_indices:
+                qubit2 = qubit_indices.pop()
+                qc.cx(qubit1, qubit2)
+
+def create_game_circuit():
+    qc = QuantumCircuit(NUM_QUBITS, NUM_QUBITS)
+    for i in range(NUM_QUBITS):
+        qc.h(i)
+    entangle_corresponding_qubits_correctly(qc, NUM_CHARACTERS, TRAITS_PER_CHARACTER)
+    return qc
+determinedTraits = {}
+for i in range(20):
+    determinedTraits[i] = None
+def measure_trait(qc, trait_index):
+    if determinedTraits[trait_index] != None:
+        return determinedTraits[trait_index],None,trait_index,None
+    group = trait_index % TRAITS_PER_CHARACTER
+    color_pair = trait_color_pairs[group]
+
+    entangled_trait_index = None
+    for instruction in qc.data:
+        if instruction[0].name == 'cx':
+            qubits = [q.index for q in instruction[1]]
+            if trait_index in qubits:
+                entangled_trait_index = qubits[1] if qubits[0] == trait_index else qubits[0]
+                break
     
-    qc.measure(qubit_to_measure, cbit)
+    if entangled_trait_index is None:
+        raise ValueError(f"Qubit {trait_index} is not entangled with any other qubit.")
+    
+    qc.measure(trait_index, trait_index)
+    qc.measure(entangled_trait_index, entangled_trait_index)
     
     simulator = Aer.get_backend('qasm_simulator')
     result = execute(qc, simulator, shots=1).result()
-    
     measurement_result = result.get_counts(qc)
-    measured_value = list(measurement_result.keys())[0][cbit]
-    return measured_value
+    outcome = list(measurement_result.keys())[0]
 
-def create_game_circuit(num_characters):
+    measured_binary = outcome[::-1][trait_index] 
+    entangled_binary = outcome[::-1][entangled_trait_index]
+
+    trait_color = color_pair[int(measured_binary)]
+    entangled_color = color_pair[1 - int(measured_binary)] 
+    determinedTraits[trait_index] = trait_color
+    determinedTraits[entangled_trait_index] = entangled_color
+    return trait_color, entangled_color, trait_index, entangled_trait_index
+
+isOver = False
+turn = 0
+peopleNumber = 4
+traitNumber = 5
+player1Matrix = np.full((traitNumber,peopleNumber),None)
+print(player1Matrix)
+qc = create_game_circuit()
+
+while isOver == False:
+    if turn == 0:
+        turn = random.randrange(0,100)%2
+    #data from daniel will be JSON with a attribute and guess
+    #measure_trait(qc,getDAtafromDaniel)
+    ##TODO:Also keep track of players idea: even numbers could be p1 and odd could be p2
+    ##TODO:Create/update matrix and update the turn
+    ##TODO: check if game is over
     
-    num_qubits = num_characters * 3
-    qc = QuantumCircuit(num_qubits, num_qubits)
-
-    for i in range(num_qubits):
-        qc.h(i)
-
-    pairs = []
-    used_qubits = set()
-    while len(pairs) < num_characters * 3 // 2: 
-        alice_qubit = random.choice([q for q in range(num_qubits) if q not in used_qubits])
-        bob_qubit = random.choice([q for q in range(num_qubits) if q not in used_qubits and q // 3 != alice_qubit // 3])
-        pairs.append((alice_qubit, bob_qubit))
-        used_qubits.update([alice_qubit, bob_qubit])
-
-    for alice_qubit, bob_qubit in pairs:
-        qc.cx(alice_qubit, bob_qubit)
-    
-    return qc, pairs
-
-num_characters = int(input("Enter the number of characters (up to 10): "))
-num_characters = min(num_characters, 20)  
-
-qc, pairs = create_game_circuit(num_characters)
-
-print("Circuit:")
 print(qc)
 
-qubit_to_measure = int(input(f"Which qubit would you like to measure (0-{num_characters*3-1})? "))
-measured_value = measure_qubit(qc, qubit_to_measure, qubit_to_measure)
-print(f"Measurement result for qubit {qubit_to_measure}: {measured_value}")
+
