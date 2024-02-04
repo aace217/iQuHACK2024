3411b9eef1b0f844d2fdd7ff406e36d2fcbb0e6a
dmo7567 <daniel.morales@yale.edu>
2024-02-04 01:56:36 -0500

    Post Test

--- frontend/RinputTest.py
+++ frontend/RinputTest.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import subprocess
+import json
 
 app = Flask(__name__,template_folder=".")
+
 @app.route("/",methods=["GET"])
 def sendData(tbd='#eb4034'):
     return render_template('rFront.html', view=[
@@ -11,5 +13,11 @@ def sendData(tbd='#eb4034'):
             [1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1]
         ])
+
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    data = request.get_json()
+    result = data.get('value')  # Assuming 'value' is the key in the sent JSON
+    return jsonify({'result': result})
 
 app.run(debug=True)
